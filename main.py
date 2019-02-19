cade5c5086e111f449d5f80a7112797c14613e6c
lin <songlin.lin@yunfangdata.com>
2019-02-19 18:08:46 +0800

    add copy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
@@ -25,7 +26,7 @@ class KeywordQueryEventListener(EventListener):
             items.append(ExtensionResultItem(icon=item['icon'],
                                              name=item['title'],
                                              description=item['subtitle'],
-                                             on_enter=HideWindowAction()))
+                                             on_enter=CopyToClipboardAction(item['title'])))
 
         return RenderResultListAction(items)
 
